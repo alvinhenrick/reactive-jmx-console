4764d34bf665906ac9375459a07a3e12eb60d37a
Alvin Henrick <share.code@aol.com>
2013-10-19 08:44:01 -0500

    integrating  open source  metis admin theme

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,10 @@ object ApplicationBuild extends Build {
   val appDependencies = Seq(
     "org.webjars" % "angularjs" % "1.1.5-1",
     "org.webjars" % "requirejs" % "2.1.8",
-    "org.webjars" % "bootstrap" % "3.0.0"  ,
+    "org.webjars" % "bootstrap" % "3.0.0" exclude("org.webjars", "jquery") ,
+    "org.webjars" % "font-awesome" % "3.2.1",
+    "org.webjars" % "modernizr" % "2.6.2-1",
+    "org.webjars" % "jquery" % "2.0.3-1",
     "org.webjars" %% "webjars-play" % "2.2.0",
     "fr.janalyse" %% "janalyse-jmx" % "0.6.3" % "compile"
   )
